6f7e4b34f0bbdae2e2bf039a089f1646b7a09c59
Michael Juliano <michaelj@sunsofterp.com>
2021-02-27 19:06:32 -0500

    Cleaned up and spec'd RuleViolation.

--- src/main/java/io/paradiddle/ms/RuleViolation.java
+++ src/main/java/io/paradiddle/ms/RuleViolation.java
@@ -16,22 +16,39 @@
  * 59 Temple Place, Suite 330
  * Boston, MA 02111-1307 USA
  */
-
 package io.paradiddle.ms;
 
 import io.paradiddle.ms.response.BadRequestResponse;
 
+/**
+ * Exception class that provides a {@link Response} that is used to reply to requesters.
+ */
 public class RuleViolation extends Exception {
+    /**
+     * The response to reply to requesters with.
+     */
     private final Response _response;
 
+    /**
+     * Constructs a {@link RuleViolation} with a default {@link Response} of
+     * {@link BadRequestResponse}.
+     */
     public RuleViolation() {
-        this._response = new BadRequestResponse();
+        this(new BadRequestResponse());
     }
 
+    /**
+     * Constructs a {@link RuleViolation} with the supplied {@link Response}.
+     * @param response The {@link Response}.
+     */
     public RuleViolation(final Response response) {
         this._response = response;
     }
 
+    /**
+     * Returns the {@link Response} for this {@link RuleViolation}.
+     * @return The {@link Response}.
+     */
     public Response response() {
         return this._response;
     }
---
+++ src/test/groovy/io/paradiddle/ms/RuleViolationSpec.groovy
@@ -0,0 +1,43 @@
+/*
+ * Paradiddle MS - A lightweight microservices library with a comprehensible codebase.
+ * Copyright (c) Michael Juliano 2021.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to:
+ *
+ * Free Software Foundation, Inc.
+ * 59 Temple Place, Suite 330
+ * Boston, MA 02111-1307 USA
+ */
+package io.paradiddle.ms
+
+import io.paradiddle.ms.response.BadRequestResponse
+import spock.lang.Specification
+
+class RuleViolationSpec extends Specification {
+    def 'provides the supplied response'() {
+        given: 'a Response'
+        def response = new BadRequestResponse()
+
+        when: 'a RuleViolation is constructed with the response'
+        def violation = new RuleViolation(response)
+
+        then: 'the RuleViolation provides the expected response'
+        violation.response().is(response)
+    }
+
+    def 'provides BadRequestResponse by default'() {
+        given: 'a RuleViolation'
+        def violation = new RuleViolation()
+
+        expect: 'the RuleViolation provides the expected response'
+        violation.response() instanceof BadRequestResponse
+    }
+}
