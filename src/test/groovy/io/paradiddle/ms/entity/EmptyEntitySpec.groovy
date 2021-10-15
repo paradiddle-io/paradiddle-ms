15f9b9d2f5e32500b003b7f3ba8354e3c2a248df
Michael Juliano <michaelj@sunsofterp.com>
2021-10-15 01:09:49 -0400

    Refined the specification for EmptyEntity.

--- src/test/groovy/io/paradiddle/ms/entity/EmptyEntitySpec.groovy
+++ src/test/groovy/io/paradiddle/ms/entity/EmptyEntitySpec.groovy
@@ -2,6 +2,7 @@ package io.paradiddle.ms.entity
 
 import io.paradiddle.ms.HeaderStore
 import io.paradiddle.ms.StandardMimeTypes
+import java.util.function.BiConsumer
 import spock.lang.Specification
 
 class EmptyEntitySpec extends Specification {
@@ -16,25 +17,53 @@ class EmptyEntitySpec extends Specification {
         entity.type() == StandardMimeTypes.NONE
     }
 
-    def 'should have no content'() {
+    def 'should have no content when consumed'() {
         given: 'an EmptyEntity'
         def entity = new EmptyEntity()
 
         when: 'the entity is consumed'
         def actualSize = 1
-        def headerCount = 1
         entity.consume(new EntityConsumer() {
             @Override
             void consume(final InputStream stream, final HeaderStore headers) throws IOException {
                 actualSize = stream.readAllBytes().size()
-                headerCount = headers.size()
             }
         })
 
         then: 'the content should have 0 bytes'
         actualSize == 0
+    }
 
-        and: 'there should be no headers'
-        headerCount == 0
+    def 'should have no content when interpreted'() {
+        given: 'an EmptyEntity'
+        def entity = new EmptyEntity()
+
+        when: 'the entity is interpreted'
+        byte[] result = entity.interpreted(new EntityDecoder<Object>() {
+            @Override
+            Object interpret(final InputStream stream, final HeaderStore store) throws IOException {
+                return stream.readAllBytes()
+            }
+        })
+
+        then: 'the content should have 0 bytes'
+        result.size() == 0
+    }
+
+    def 'should have no headers'() {
+        given: 'an EmptyEntity'
+        def entity = new EmptyEntity()
+
+        when: 'the headers are consumed'
+        def results = [:]
+        entity.consumeHeaders(new BiConsumer<String, String>() {
+            @Override
+            void accept(final String name, final String value) {
+                results.put(name, value)
+            }
+        })
+
+        then: 'the results should be empty'
+        results.size() == 0
     }
 }
